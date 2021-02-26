ec98524e9c6dd4857156f6d350cd00bfe66f3d4e
pprg1996 <pprg1996@gmail.com>
2021-02-26 06:56:49 -0400

    SwitchToggle mejorado Acepta tw via props Funciona aun si hay varios switches presente

--- src/shared/components/SwitchToggle.tsx
+++ src/shared/components/SwitchToggle.tsx
@@ -1,31 +1,39 @@
-import tw from "twin.macro";
+import tw, { TwStyle } from "twin.macro";
 
-const SwitchToggle = ({ checked, setChecked, label }: { checked: boolean; setChecked: Function; label: string }) => {
+const SwitchToggle = ({
+  checked,
+  setChecked,
+  label,
+  wrapperTW,
+}: {
+  checked: boolean;
+  setChecked: Function;
+  label: string;
+  wrapperTW?: TwStyle;
+}) => {
   return (
-    <div className="switch-toggle">
+    <label className="switch-toggle" tw="text-xs text-gray-700" css={[wrapperTW]}>
       <div tw="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
-        <input
-          type="checkbox"
-          name="toggle"
-          id="toggle"
-          tw="absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"
-          css={[checked ? tw`right-0 border-green-400` : null]}
-          className="toggle-checkbox"
-          onChange={e => setChecked(e.currentTarget.checked)}
-          checked={checked}
-        />
         <label
-          htmlFor="toggle"
           tw="block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"
           css={[checked ? tw`bg-green-400` : null]}
           className="toggle-label"
-        ></label>
+        >
+          <input
+            type="checkbox"
+            name="toggle"
+            id="toggle"
+            tw="absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"
+            css={[checked ? tw`right-0 border-green-400` : null]}
+            className="toggle-checkbox"
+            onChange={e => setChecked(e.currentTarget.checked)}
+            checked={checked}
+          />
+        </label>
       </div>
 
-      <label htmlFor="toggle" tw="text-xs text-gray-700">
-        {label}
-      </label>
-    </div>
+      {label}
+    </label>
   );
 };
 
