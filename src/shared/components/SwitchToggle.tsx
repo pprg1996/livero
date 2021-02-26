a2145fdaf47c739c6822e113fde10abe8797e7b1
pprg1996 <pprg1996@gmail.com>
2021-02-26 05:51:01 -0400

    Mejorada implementacion de SwitchToggle

--- src/shared/components/SwitchToggle.tsx
+++ src/shared/components/SwitchToggle.tsx
@@ -1,24 +1,15 @@
-import tw, { styled } from "twin.macro";
-
-const Input = styled.input`
-  :checked {
-    ${tw`right-0 border-green-400`}
-  }
-
-  :checked + .toggle-label {
-    ${tw`bg-green-400`}
-  }
-`;
+import tw from "twin.macro";
 
 const SwitchToggle = ({ checked, setChecked, label }: { checked: boolean; setChecked: Function; label: string }) => {
   return (
     <div>
       <div tw="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
-        <Input
+        <input
           type="checkbox"
           name="toggle"
           id="toggle"
           tw="absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"
+          css={[checked ? tw`right-0 border-green-400` : null]}
           className="toggle-checkbox"
           onChange={e => setChecked(e.currentTarget.checked)}
           checked={checked}
@@ -26,6 +17,7 @@ const SwitchToggle = ({ checked, setChecked, label }: { checked: boolean; setChe
         <label
           htmlFor="toggle"
           tw="block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"
+          css={[checked ? tw`bg-green-400` : null]}
           className="toggle-label"
         ></label>
       </div>
