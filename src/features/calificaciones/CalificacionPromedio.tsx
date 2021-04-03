de10499d25dc6f89bcd8a60c807364114b5f2c84
pprg1996 <pprg1996@gmail.com>
2021-04-02 20:35:15 -0400

    Popup de criticas

--- src/features/calificaciones/CalificacionPromedio.tsx
+++ src/features/calificaciones/CalificacionPromedio.tsx
@@ -1,7 +1,13 @@
 import { Calificacion } from "features/tienda/types";
-import { FC } from "react";
+import { FC, useRef, useState } from "react";
+import { useClickOutside } from "shared/hooks";
 
 const CalificacionPromedio: FC<{ calificaciones: Record<string, Calificacion> | undefined }> = ({ calificaciones }) => {
+  const [showCriticas, setShowCriticas] = useState(false);
+  const btnRef = useRef<HTMLButtonElement>(null);
+
+  useClickOutside(btnRef, () => setShowCriticas(false));
+
   if (!calificaciones)
     return <span tw="font-medium text-gray-700 underline text-underline-position[under]">Sin calificaciones</span>;
 
@@ -12,10 +18,26 @@ const CalificacionPromedio: FC<{ calificaciones: Record<string, Calificacion> | 
       return totalEstrellas + calificacion.estrellas;
     }, 0) / calificacionesTuple.length;
 
+  const criticasTuple = calificacionesTuple
+    .filter(([, calificacion]) => calificacion.critica)
+    .map(([id, calificacion]) => [id, calificacion.critica]);
   return (
-    <span tw="font-medium text-gray-700 underline text-underline-position[under]">
+    <button
+      onClick={() => setShowCriticas(true)}
+      tw="relative font-medium text-gray-700 underline text-underline-position[under]"
+      ref={btnRef}
+    >
       Calificación {estrellasPromedio.toFixed(1)}⭐
-    </span>
+      {showCriticas ? (
+        <div tw="absolute left-1/2 transform -translate-x-1/2 top-5 bg-white shadow border p-2 rounded max-h-32 w-52 overflow-auto flex flex-col divide-y">
+          {criticasTuple.length ? (
+            criticasTuple.map(([id, critica]) => <span key={id}>{critica}</span>)
+          ) : (
+            <span>Sin reseñas</span>
+          )}
+        </div>
+      ) : null}
+    </button>
   );
 };
 
